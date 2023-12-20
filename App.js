8e0e4ecb887e353df921416d70ffb5b9c171c50b
“itzkennedydev” <Kennedy@itskennedy.dev>
2023-12-20 02:14:13 -0600

    Navigation

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-import {Text, SafeAreaView} from 'react-native';
 import React from 'react';
+import AppNavigation from './src/navigation';
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <Text>App</Text>
-    </SafeAreaView>
-  );
+  return <AppNavigation />;
 }
